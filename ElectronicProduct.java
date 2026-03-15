20720b38470d4c1dbf168cadca25854ab96f0674
MuhammadHafizhRaihan <hafizhb9@gmail.com>
2026-03-16 06:45:58 +0700

    Menyelesaikan subclass ElectronicProduct dan FoodProduct

--- ElectronicProduct.java
+++ ElectronicProduct.java
@@ -1,9 +1,43 @@
 public class ElectronicProduct extends Product {
-    // TODO: Anggota 2 - Tambahkan atribut warrantyPeriod & Constructor
+    // Tambahkan atribut warrantyPeriod & Constructor
+    private String warrantyPeriod;
+
+    public ElectronicProduct(){
+        super();
+        this.warrantyPeriod = "Unknown";
+    }
+
+    public ElectronicProduct(String productId, String name, double price, int stockQuantity, String warrantyPeriod) {
+        super(productId, name, price, stockQuantity);
+        this.warrantyPeriod = warrantyPeriod;
+    }
+
+    public String getWarrantyPeriod() {
+        return warrantyPeriod;
+    }
+
+    public void setWarrantyPeriod(String warrantyPeriod) {
+        this.warrantyPeriod = warrantyPeriod;
+    }
     
     @Override
     public double calculateDiscount() {
-        // TODO: Anggota 2 - Buat logika diskon 5% + bonus 2% jika harga > 500rb
-        return 0;
+        // Buat logika diskon 5% + bonus 2% jika harga > 500rb
+         double discount = getPrice() * 0.05;
+
+        if (getPrice() > 500000) {
+            discount += getPrice() * 0.02;
+        }
+
+        return discount;
+    }
+
+    @Override
+    public void getProductInfo(){
+        super.getProductInfo();
+        System.out.println("Warranty period : " + this.warrantyPeriod);
+        System.out.println("Diskon         : Rp " + calculateDiscount());
+        System.out.println("Harga Akhir    : Rp " + (getPrice()-calculateDiscount()));
+
     }
 }
--- FoodProduct.java
+++ FoodProduct.java
@@ -1,9 +1,42 @@
 public class FoodProduct extends Product {
-    // TODO: Anggota 2 - Tambahkan atribut expiryDate & Constructor
-    
+    // Tambahkan atribut expiryDate & Constructor
+    private String expiryDate;
+
+    public FoodProduct(){
+        super();
+        this.expiryDate = "unknown";
+    }
+
+    public FoodProduct(String productId, String name, double price, int stockQuantity, String expiryDate) {
+        super(productId, name, price, stockQuantity);
+        this.expiryDate = expiryDate;
+    }
+
+ public String getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(String expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+
     @Override
     public double calculateDiscount() {
-        // TODO: Anggota 2 - Buat logika diskon 10% jika stok > 10
+        // Buat logika diskon 10% jika stok > 10
+         if (getStockQuantity() > 10) {
+            return getPrice() * 0.10;
+        }
         return 0;
     }
+
+    @Override
+    public void getProductInfo(){
+        super.getProductInfo();
+        System.out.println("Tanggal Expiry : " + this.expiryDate);
+        System.out.println("Diskon         : Rp " + calculateDiscount());
+        System.out.println("Harga Akhir    : Rp " + (getPrice()-calculateDiscount()));
+    }
+
+
 }
