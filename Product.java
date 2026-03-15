269b402ea704a634845ab4c91596df5c43a321e4
Hafiizhanindyaa <hafiizhanindyaaa@gmail.com>
2026-03-16 00:48:03 +0700

    Menyelesaikan Class Product dengan Encapsulation, Constructor, dan Overloading

--- Product.java
+++ Product.java
@@ -1,22 +1,80 @@
 public abstract class Product {
+    // 1. Atribut private (Enkapsulasi)
     private String productId;
     private String name;
     private double price;
     private int stockQuantity;
 
-    // TODO: Anggota 1 - Buat Constructor, Getter, dan Setter
+    // 2. Konstruktor Default
+    public Product() {
+        this.productId = "P000";
+        this.name = "Unknown";
+        this.price = 0.0;
+        this.stockQuantity = 0;
+    }
 
+    // 3. Konstruktor Parameterized
+    public Product(String productId, String name, double price, int stockQuantity) {
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+
+    // 4. Getter dan Setter (Enkapsulasi)
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+    // 5. Method Abstrak untuk menghitung diskon
     public abstract double calculateDiscount();
 
+    // 6. Method menampilkan informasi produk dasar
     public void getProductInfo() {
-        // TODO: Anggota 1 - Isi cetakan info produk
+        System.out.println("ID Produk   : " + productId);
+        System.out.println("Nama Produk : " + name);
+        System.out.println("Harga       : Rp " + price);
+        System.out.println("Sisa Stok   : " + stockQuantity);
     }
 
+    // 7. Method Overloading: updateStock hanya dengan kuantitas
     public void updateStock(int quantity) {
-        // TODO: Anggota 1 - Isi logika tambah stok
+        this.stockQuantity += quantity;
+        System.out.println("Stok " + this.name + " berhasil diupdate. Stok sekarang: " + this.stockQuantity);
     }
 
+    // 8. Method Overloading: updateStock dengan kuantitas dan alasan
     public void updateStock(int quantity, String reason) {
-        // TODO: Anggota 1 - Isi logika tambah stok dengan alasan
+        this.stockQuantity += quantity;
+        System.out.println("Stok " + this.name + " berhasil diupdate. Stok sekarang: " + this.stockQuantity);
+        System.out.println("Alasan update: " + reason);
     }
 }
