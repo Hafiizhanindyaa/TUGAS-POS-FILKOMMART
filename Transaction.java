acf3bdff78e79dc8f58263fc67ea18d5e2dbb4c8
HAFIZ ADLI ASSYAHADAT <hafiz.adli.assyahadat@gmail.com>
2026-03-16 07:52:30 +0700

    Menambahkan Class Transaction menggunakan ArrayList dan implementasi Method Overloading pada addItem

--- Transaction.java
+++ Transaction.java
@@ -1,9 +1,74 @@
+import java.util.ArrayList;
+
 public class Transaction {
-    // TODO: Anggota 3 - Tambahkan atribut transactionId, items, totalItems
-    // TODO: Anggota 3 - Buat Method overloading addItem
-    
-    public double processSale() {
-        // TODO: Anggota 3 - Hitung total harga setelah diskon
-        return 0;
+    private ArrayList<Product> items;
+    private int totalItems;
+
+    // Constructor untuk inisialisasi
+    public Transaction() {
+        this.items = new ArrayList<>();
+        this.totalItems = 0;
+    }
+
+    // Method Overloading: addItem dengan 1 parameter (1 produk)
+    public void addItem(Product item) {
+        this.items.add(item);
+        this.totalItems++;
+        System.out.println(item.getName() + " berhasil ditambahkan ke keranjang.");
+    }
+
+    // Method Overloading: addItem dengan 2 parameter (produk dan jumlah kuantitas)
+    public void addItem(Product item, int quantity) {
+        for (int i = 0; i < quantity; i++) {
+            this.items.add(item);
+        }
+        this.totalItems += quantity;
+        System.out.println(quantity + " " + item.getName() + " berhasil ditambahkan ke keranjang.");
+    }
+
+    // Method processSale() untuk memproses keranjang dan menghitung total
+    public void processSale() {
+        System.out.println("\n========================================");
+        System.out.println("             STRUK PEMBELIAN            ");
+        System.out.println("========================================");
+        
+        double totalHargaAwal = 0.0;
+        double totalDiskon = 0.0;
+        double totalBayar = 0.0;
+
+        for (Product item : items) {
+            double harga = item.getPrice();
+            double diskon = item.calculateDiscount(); 
+            double hargaSetelahDiskon = harga - diskon;
+
+            System.out.println("- " + item.getName());
+            System.out.println("  Harga   : Rp " + harga);
+            if (diskon > 0) {
+                System.out.println("  Diskon  : Rp " + diskon);
+            }
+            System.out.println("  Subtotal: Rp " + hargaSetelahDiskon);
+            
+            totalHargaAwal += harga;
+            totalDiskon += diskon;
+            totalBayar += hargaSetelahDiskon;
+
+            item.setStockQuantity(item.getStockQuantity() - 1);
+        }
+
+        System.out.println("----------------------------------------");
+        System.out.println("Total Item    : " + totalItems);
+        System.out.println("Total Belanja : Rp " + totalHargaAwal);
+        System.out.println("Total Diskon  : Rp " + totalDiskon);
+        System.out.println("Total Bayar   : Rp " + totalBayar);
+        System.out.println("========================================\n");
+    }
+
+    // Method tambahan (Getter) agar bisa mengambil total bayar dari setiap transaksi
+    public double getTotalTransactionAmount() {
+        double total = 0.0;
+        for (Product item : items) {
+            total += (item.getPrice() - item.calculateDiscount());
+        }
+        return total;
     }
 }
